b8c541c9dc45b766245de46f8113da221f565217
ACBennny <acbennny1@gmail.com>
2024-10-28 01:25:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ const struct =
                                             </svg>
                                             <p class="media_optText">Download</p>
                                         </button>
-                                        <button type="button" class="media_optBox shareShowBtn" title="Add to Watchlist" aria-label="Add to Watchlist">
+                                        <button type="button" class="media_optBox shareShowBtn" title="Share episode" aria-label="Share episode">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="media_optIcon add_to_WatchListIcon">
                                                 <path xmlns="http://www.w3.org/2000/svg" d="M384 336a63.78 63.78 0 00-46.12 19.7l-148-83.27a63.85 63.85 0 000-32.86l148-83.27a63.8 63.8 0 10-15.73-27.87l-148 83.27a64 64 0 100 88.6l148 83.27A64 64 0 10384 336z"/>
                                             </svg>
